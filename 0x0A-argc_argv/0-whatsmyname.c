412496549130cada04c7daa65ad2c9ec5d099b3f
“bisrath” <bisrat_hl@yahoo.com>
2023-09-02 00:50:19 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -5,17 +5,17 @@
 
 /**
 
- * main - prints the name of the program
+* main - prints the name of the program
 
- * @argc: number of arguments
+* @argc: number of arguments
 
- * @argv: array of arguments
+* @argv: array of arguments
 
- *
+*
 
- * Return: Always 0 (Success)
+* Return: Always 0 (Success)
 
- */
+*/
 
 int main(int argc __attribute__((unused)), char *argv[])
 
@@ -26,4 +26,3 @@ int main(int argc __attribute__((unused)), char *argv[])
 
 	return (0);
 }
-
